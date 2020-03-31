cd76d0ed2fd636a607392d137d2f9f288a5cc294
Syra-Nyth <48935704+Syra-Nyth@users.noreply.github.com>
2020-03-31 18:51:07 +1300

    Hot Chocolate

--- src/index.svelte
+++ src/index.svelte
@@ -1,3 +1,27 @@
-<h1>Svelte</h1>
+<script>
+    let name = `What's your name?`
+    let tempC = 20
+    $:tempF = Math.round(tempC * 1.8) + 32
+</script>
 
-<p>Welcome to coding with Svelte!</p>
+<h1 class="title">Hot Chocolate</h1>
+
+<label>
+    Name: 
+    <input type="text" bind:value={name} />
+</label>
+
+<label>
+    Drink temperature: 
+    <input type="range" min="0" max="100" bind:value={tempC} />
+</label>
+
+{tempC}°C ({tempF}°F)
+
+{#if tempC < 40}
+    <p>A wee bit chilly ❄</p>
+{:else if tempC > 90}
+    <p>Ouch! Burnt tongue 🔥.</p>
+{:else}
+    <p>Very nice! ☕</p>
+{/if}
